8c90de307b064129af9abb547720f0d06983324c
acrochet95 <acrochet95@gmail.com>
2016-03-09 20:55:22 +0100

    Modif menu principal + ajout activité stats

--- app/src/main/java/fr/insa_rennes/greensa/MainActivity.java
+++ app/src/main/java/fr/insa_rennes/greensa/MainActivity.java
@@ -1,13 +1,39 @@
 package fr.insa_rennes.greensa;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import fr.insa_rennes.greensa.database.controller.CourseDAO;
+import fr.insa_rennes.greensa.database.model.Course;
+
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*
+        CourseDAO coursebd = new CourseDAO(this);
+        coursebd.open();
+        coursebd.ajouter(new Course(2,9, 0.5f, 0.5f));
+        coursebd.close();
+        */
+
+        TextView newGame = (TextView)findViewById(R.id.newGame);
+        TextView stats = (TextView)findViewById(R.id.stats);
+        TextView help = (TextView)findViewById(R.id.help);
+
+        stats.setOnClickListener(new View.OnClickListener() {
+
+            public void onClick(View v) {
+                Intent activity = new Intent(MainActivity.this, Stats.class);
+                startActivity(activity);
+            }
+
+        });
     }
 }
---
+++ app/src/main/java/fr/insa_rennes/greensa/Stats.java
@@ -0,0 +1,13 @@
+package fr.insa_rennes.greensa;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Stats extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_stats);
+    }
+}
